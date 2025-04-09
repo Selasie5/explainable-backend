f458c31fff0d8a3a93caa862d005a31b09081fd7
Selasie5 <selasisepenu5@gmail.com>
2025-04-09 11:05:34 +0000

    Implement FastAPI application with root and item retrieval endpoints

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_items(item_id):
+    return {"item_id":item_id}
