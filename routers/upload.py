78f210c681269e30b92011ecde12918cffed724c
Selasie5 <selasisepenu5@gmail.com>
2025-04-13 14:54:09 +0000

    feat:Enhance file upload endpoint to support multiple file uploads with validation

--- routers/upload.py
+++ routers/upload.py
@@ -1,11 +1,20 @@
+from typing import List  
 from fastapi import APIRouter, UploadFile, File, HTTPException, status
 from utils.file_checker import validate_file_type
 
 UploadFileRouter = APIRouter()
+
 @UploadFileRouter.post("/upload-dataset")
-async def upload_file(file: UploadFile = File(...)):
-    try:
-        ext = validate_file_type(file)
-        return {"filename": file.filename, "content_type": file.content_type, "extension": ext}
-    except HTTPException as e:
-        raise e
+async def upload_file(files: List[UploadFile] = File(...)):  
+    results = []
+    for file in files:
+        try:
+            ext = validate_file_type(file)
+            results.append({
+                "filename": file.filename,
+                "content_type": file.content_type,
+                "extension": ext
+            })
+        except HTTPException as e:
+            raise e
+    return results
